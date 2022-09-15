b24bd93c53915db0cdd4df2cbe1a87460bd8511d
MostafaHQ <mostafa.hajqasem@gmail.com>
2022-09-15 13:20:11 +0300

    flask1

---
+++ _python/python_fundamentals/assignments/functions_Basic_1/python.py
@@ -0,0 +1,17 @@
+#6
+def a(b,c):
+    print(b+c)
+print(a(1,2) + a(2,3))
+
+
+# #8
+# def a():
+#     b = 100
+#     print(b)
+#     if b < 10:
+#         return 5
+#     else:
+#         return 10
+#     return 7
+# print(a())
+

9b46a7b8416d8af0e2a5432a34fd20b5a7461515
MostafaHQ <mostafa.hajqasem@gmail.com>
2022-09-18 21:34:24 +0300

    playground

---
+++ _python/python_fundamentals/assignments/Playground/server.py
@@ -0,0 +1,23 @@
+from os import times
+from flask import Flask, render_template
+
+playground_app = Flask(__name__)
+
+
+@playground_app.route("/play")
+def play():
+    return render_template("Index1.html")
+
+
+@playground_app.route("/play/<times>")
+def numbers_box(times):
+    return render_template("Index2.html", num_box=int(times))
+
+
+@playground_app.route("/play/<times>/<color_cahnge>")
+def colorCange(times, color_cahnge):
+    return render_template("Index3.html", num_box=int(times), color_box=color_cahnge)
+
+
+if __name__ == "__main__":
+    playground_app.run(debug=True)

fa05dec64a22018509fa90a613d07655988f9910
MostafaHQ <mostafa.hajqasem@gmail.com>
2022-09-24 09:03:11 +0300

    ready

--- _python/python_fundamentals/assignments/Checkerboard/server.py
+++ _python/python_fundamentals/assignments/Checkerboard/server.py
@@ -5,12 +5,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def checkerBoard8():
-    return render_template("Index.html")
+    return render_template("Index.html", x=8, y=4, color_one="black", color_two="red")
 
 
-# @app.route("/4")
-# def checkerBoard4():
-#     return render_template("Index.html")
+@app.route("/<x>")
+def checkerBoardx(x):
+    return render_template("Index.html", x=int(x), y=4, color_one="black", color_two="red")
+
+
+@app.route("/<x>/<y>")
+def checkerBoardxy(x, y):
+    return render_template("Index.html", x=int(x), y=int(y), color_one="black", color_two="red")
+
+
+# @app.route("/<x>/<y>/<color1>/<color2>")
+# def checkerBoardxyColoring(x, y, color1, color2):
+#     return render_template("Index.html", x=int(x), y=int(y), color_one=color1, color_two=color2)
 
 
 if __name__ == "__main__":
